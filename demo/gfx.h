0679f460f5ca229d7fb2313ecaba5eb8101c21bb
Lucas Michaudel <lucas.michaudel@gmail.com>
2025-05-15 19:25:37 +0200

    feat: screenshot

--- demo/gfx.h
+++ demo/gfx.h
@@ -1,7 +1,10 @@
 #ifndef TIPE_GFX
 #define TIPE_GFX
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "rect.h"
+#include "stb_image_write.h"
 #include <SDL.h>
 #include <stdbool.h>
 
@@ -28,7 +31,9 @@ static bool gfx_init(Graphics* ctx, const char* title, int width, int height) {
   }
 
   ctx->renderer = SDL_CreateRenderer(
-      ctx->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+      ctx->window, -1,
+      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC |
+          SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
   SDL_SetRenderDrawBlendMode(ctx->renderer, SDL_BLENDMODE_BLEND);
 
   if (ctx->renderer == NULL) {
@@ -76,6 +81,41 @@ static void gfx_draw_circle(Graphics* ctx, int cx, int cy, int radius,
       }
     }
   }
+}
+
+static void save_screenshot(Graphics* ctx, const char* filename) {
+  int w, h;
+  SDL_GetRendererOutputSize(ctx->renderer, &w, &h);
+
+  size_t buffer_size = w * h * 4;
+  unsigned char* pixels = malloc(buffer_size);
+  unsigned char* converted = malloc(buffer_size);
+  if (!pixels || !converted) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return;
+  }
+
+  if (SDL_RenderReadPixels(ctx->renderer, NULL, SDL_PIXELFORMAT_RGBA32, pixels,
+                           w * 4) != 0) {
+    fprintf(stderr, "SDL_RenderReadPixels failed: %s\n", SDL_GetError());
+    free(pixels);
+    free(converted);
+    return;
+  }
+
+  // Swizzle BGRA -> RGBA
+  for (int i = 0; i < w * h; ++i) {
+    converted[i * 4 + 0] = pixels[i * 4 + 0]; // R
+    converted[i * 4 + 1] = pixels[i * 4 + 1]; // G
+    converted[i * 4 + 2] = pixels[i * 4 + 2]; // B
+    converted[i * 4 + 3] = 255;               // A
+  }
+
+  stbi_write_png(filename, w, h, 4, converted, w * 4);
+
+  printf("Saved screenshot.\n");
+  free(pixels);
+  free(converted);
 }
 
 #endif
